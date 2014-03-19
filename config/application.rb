18e96768eccb764a1854712d3f825a51114d1c6a
IggyIgner <jonathan.igner@gmail.com>
2014-03-18 17:45:08 -0700

    make paperclip + Cloud Storage work!

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,19 @@ module Project4
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Defaults for our Paperclip images
+    config.paperclip_defaults = { 
+      storage: :fog,
+      fog_credentials: {
+        provider: "Google",
+        google_storage_access_key_id: ENV['GSTORAGE_ACCESS_KEY_ID'], #whenever see ENV and [], replaces with what's in your application.yml file
+        google_storage_secret_access_key: ENV['GSTORAGE_SECRET_ACCESS_KEY']
+      },
+      fog_public: true,
+      fog_host: ENV['GSTORAGE_HOST'],
+      fog_directory: ENV['GSTORAGE_BUCKET'],
+      path: ':class/:attachment/:id/:style/:filename' #:class is user/:attachment/:id is id of photo/:style is dimensiosn we spec'd/:filename
+    }
   end
 end
