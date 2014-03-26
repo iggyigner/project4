ee0bfc90ba8d18ca564b30c57bf331108804efad
RachelBailin <rachelebailin@gmail.com>
2014-03-26 15:45:16 -0700

    heroku deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Project4::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  Paperclip.options[:command_path] = "/usr/local/bin/"
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -18,6 +20,9 @@ Project4::Application.configure do
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
+
+  Paperclip.options[:command_path] = 'C:/Progra~1/ImageM~1.8-q'
+
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
